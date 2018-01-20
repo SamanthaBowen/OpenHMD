5124cc51fe6d14cf493f3a9d8e81ca54d1718601
Christoph Haag <haagch@frickel.club>
2018-01-20 14:51:08 +0100

    define USE_DOUBLE for survive.h to silence warnings
    also explicitely cast to FLT (hopefully OpenHMD's version of the define)

--- src/drv_htc_vive/vive.c
+++ src/drv_htc_vive/vive.c
@@ -18,8 +18,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "vive.h"
+#define USE_DOUBLE
 #include <survive.h>
+#undef USE_DOUBLE
+
+#include "vive.h"
 
 typedef struct {
 	ohmd_device base;
@@ -41,16 +44,16 @@ static int getf(ohmd_device* device, ohmd_float_value type, float* out)
 	case OHMD_ROTATION_QUAT:
 		// libsurvive_quat is 0=w 1=x 2=y 3=z
 		// out is 0=x 1=y 2=z 3=w
-		out[0] = priv->libsurvive_quat[1];
-		out[1] = priv->libsurvive_quat[2];
-		out[2] = priv->libsurvive_quat[3];
-		out[3] = priv->libsurvive_quat[0];
+		out[0] = (FLT) priv->libsurvive_quat[1];
+		out[1] = (FLT) priv->libsurvive_quat[2];
+		out[2] = (FLT) priv->libsurvive_quat[3];
+		out[3] = (FLT) priv->libsurvive_quat[0];
 		break;
 
 	case OHMD_POSITION_VECTOR:
-		out[0] = priv->libsurvive_pos[0];
-		out[1] = priv->libsurvive_pos[1];
-		out[2] = priv->libsurvive_pos[2];
+		out[0] = (FLT) priv->libsurvive_pos[0];
+		out[1] = (FLT) priv->libsurvive_pos[1];
+		out[2] = (FLT) priv->libsurvive_pos[2];
 		break;
 
 	case OHMD_DISTORTION_K:
